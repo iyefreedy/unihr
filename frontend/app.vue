913e32aed65718d217bbff5172f2ab5f1a37543b
Muhammad Quraisy <quraisy@uai.ac.id>
2023-11-16 16:03:23 +0700

    change app config

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,20 +1,17 @@
 <template>
     <Head>
         <Link rel="preconnect" href="https://fonts.googleapis.com" />
-        <Link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossorigin="anonymous"
-        />
+        <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 
         <Link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
-            rel="stylesheet"
-        />
+            rel="stylesheet" />
         <Title>UniHR</Title>
     </Head>
+
     <Body class="antialiased">
         <NuxtLayout>
+            <NuxtLoadingIndicator />
             <NuxtPage />
         </NuxtLayout>
 
--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -1,23 +1,44 @@
 <template>
-    <UCard>
-        <template #header>
-            <h1 class="mb-2">Welcome back, Muhammad Quraisy!</h1>
-            <p class="mb-4 text-sm text-slate-600 dark:text-slate-300">
-                It's Monday, 13 November
-            </p>
-            <div class="flex items-center space-x-4">
-                <UButton label="Request time off" variant="outline" />
-                <UButton label="Request overtime" variant="outline" />
-                <UDropdown :items="items" :popper="{ arrow: true }">
-                    <UButton
-                        variant="outline"
-                        label="More requests"
-                        trailing-icon="i-heroicons-chevron-down-20-solid"
-                    />
-                </UDropdown>
-            </div>
-        </template>
-    </UCard>
+    <div class="flex flex-wrap space-y-4">
+        <UCard class="flex-[0_0_auto] w-full">
+            <main class="space-y-10">
+                <div>
+                    <h1 class="mb-2">Welcome back, <br class="md:hidden" />Muhammad Quraisy!</h1>
+                    <p class="text-sm text-slate-600 dark:text-slate-300">
+                        It's Monday, 13 November
+                    </p>
+                </div>
+
+                <div class="flex items-center py-4 space-x-4 overflow-x-scroll">
+                    <UButton label="Request time off" variant="outline" />
+                    <UButton label="Request overtime" variant="outline" />
+                    <UDropdown :items="items" :popper="{ arrow: true }">
+                        <UButton variant="outline" label="More requests"
+                            trailing-icon="i-heroicons-chevron-down-20-solid" />
+                    </UDropdown>
+                </div>
+            </main>
+        </UCard>
+        <UCard class="flex-[0_0_auto] w-1/3">
+            <main class="space-y-10">
+                <UMeterGroup :max="320">
+                    <template #indicator>
+                        <div class="flex gap-1.5 justify-between text-sm">
+                            <p>Total</p>
+                            <p class="text-gray-500 dark:text-gray-400">
+                                320 employees
+                            </p>
+                        </div>
+                    </template>
+
+                    <UMeter :value="250" color="green" label="Permanent" icon="i-heroicons-cog-6-tooth" />
+                    <UMeter :value="40" color="yellow" label="Probation" icon="i-heroicons-window" />
+                    <UMeter :value="10" color="red" label="Contract" icon="i-heroicons-document" />
+                    <UMeter :value="20" color="gray" label="Intern" icon="i-heroicons-film" />
+                </UMeterGroup>
+            </main>
+        </UCard>
+    </div>
 </template>
 
 <script lang="ts" setup>
