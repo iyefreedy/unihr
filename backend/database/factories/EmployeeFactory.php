f7d8e2676a8e29d37a3bfe02b59b3c0e0666f5b2
Muhammad Quraisy <quraisy@uai.ac.id>
2023-11-19 01:33:04 +0700

    feat(factory): add employees factory

--- backend/database/factories/EmployeeFactory.php
+++ backend/database/factories/EmployeeFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Faker\Provider\id_ID\Person;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,19 @@ class EmployeeFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = \Faker\Factory::create('id_ID');
+        $gender = $faker->randomElement([Person::GENDER_MALE, Person::GENDER_FEMALE]);
+
         return [
-            //
+            'identity_number' => $faker->nik(),
+            'gender' => $gender,
+            'birth_date' => $faker->date('Y-m-d', '2004-12-31'),
+            'birth_place' => $faker->city(),
+            'address' => $faker->streetAddress(),
+            'phone' => $faker->phoneNumber(),
+            'postal_code' => $faker->postcode(),
+            'city' => $faker->city(),
+            'province' => $faker->country(),
         ];
     }
 }
