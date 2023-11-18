036b5ca6cf548828b4d6ffc942a7956eec2a50d3
Muhammad Quraisy <quraisy@uai.ac.id>
2023-11-18 23:00:35 +0700

    feat(level): add employee level pages

--- frontend/pages/levels/index.vue
+++ frontend/pages/levels/index.vue
@@ -1,14 +1,74 @@
 <template>
-  <div>
-    
-  </div>
+    <div>
+        <UCard>
+            <template #header>
+                <h1>Levels</h1>
+                <span class="text-xs"
+                    >Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Dolorem, praesentium. Qui praesentium facere inventore
+                    similique!</span
+                >
+            </template>
+
+            <main>
+                <UTable :rows="positions" :columns="columns">
+                    <template #parent-data="{ row }">
+                        {{ row.parent ?? "No parent positions" }}
+                    </template>
+
+                    <template #actions-data="{ row }">
+                        <UDropdown :items="actionItems(row)">
+                            <UButton
+                                color="gray"
+                                variant="ghost"
+                                icon="i-heroicons-ellipsis-horizontal-20-solid"
+                            />
+                        </UDropdown>
+                    </template>
+                </UTable>
+            </main>
+        </UCard>
+    </div>
 </template>
 
 <script lang="ts" setup>
+const columns = [
+    {
+        key: "name",
+        label: "Name",
+    },
 
+    {
+        key: "actions",
+    },
+];
+const positions = [
+    {
+        name: "Director",
+    },
+    {
+        name: "Deputy Director",
+    },
+    {
+        name: "Head of Division",
+    },
+    {
+        name: "Head of Subsection",
+    },
+];
+
+const actionItems = (row: any) => [
+    [
+        {
+            label: "Edit",
+            icon: "i-mdi-database-edit",
+        },
+        {
+            label: "Delete",
+            icon: "i-mdi-trash-can",
+        },
+    ],
+];
 </script>
 
-<style>
-
-</style>
-
+<style></style>
