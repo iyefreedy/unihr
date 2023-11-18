ade9347f4d4a9e72cb597d52facf4c8a7c1db2a9
Muhammad Quraisy <quraisy@uai.ac.id>
2023-11-18 22:59:09 +0700

    feat(chart): add additional chart display (gender, employee status, etc.)

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -1,47 +1,101 @@
 <template>
-    <div class="flex flex-wrap space-y-4">
-        <UCard class="flex-[0_0_auto] w-full">
-            <main class="space-y-10">
-                <div>
-                    <h1 class="mb-2">Welcome back, <br class="md:hidden" />Muhammad Quraisy!</h1>
-                    <p class="text-sm text-slate-600 dark:text-slate-300">
-                        It's Monday, 13 November
-                    </p>
-                </div>
+    <div class="grid grid-cols-4">
+        <div class="col-span-4 w-full p-3">
+            <UCard>
+                <main class="space-y-10">
+                    <div>
+                        <h1 class="mb-2">
+                            Welcome back, <br class="md:hidden" />Muhammad
+                            Quraisy!
+                        </h1>
+                        <p class="text-sm text-slate-600 dark:text-slate-300">
+                            It's Monday, 13 November
+                        </p>
+                    </div>
 
-                <div class="flex items-center py-4 space-x-4 overflow-x-scroll">
-                    <UButton label="Request time off" variant="outline" />
-                    <UButton label="Request overtime" variant="outline" />
-                    <UDropdown :items="items" :popper="{ arrow: true }">
-                        <UButton variant="outline" label="More requests"
-                            trailing-icon="i-heroicons-chevron-down-20-solid" />
-                    </UDropdown>
-                </div>
-            </main>
-        </UCard>
-        <UCard class="flex-[0_0_auto] w-1/3">
-            <main class="space-y-10">
-                <UMeterGroup :max="320">
-                    <template #indicator>
-                        <div class="flex gap-1.5 justify-between text-sm">
-                            <p>Total</p>
-                            <p class="text-gray-500 dark:text-gray-400">
-                                320 employees
-                            </p>
-                        </div>
-                    </template>
+                    <div
+                        class="flex items-center py-4 space-x-4 overflow-x-scroll"
+                    >
+                        <UButton label="Request time off" variant="outline" />
+                        <UButton label="Request overtime" variant="outline" />
+                        <UDropdown :items="items" :popper="{ arrow: true }">
+                            <UButton
+                                variant="outline"
+                                label="More requests"
+                                trailing-icon="i-heroicons-chevron-down-20-solid"
+                            />
+                        </UDropdown>
+                    </div>
+                </main>
+            </UCard>
+        </div>
+        <div class="col-span-2 p-3">
+            <UCard class="h-full">
+                <main class="space-y-10">
+                    <UMeterGroup :max="320">
+                        <template #indicator>
+                            <div class="flex gap-1.5 justify-between text-sm">
+                                <p>Total</p>
+                                <p class="text-gray-500 dark:text-gray-400">
+                                    320 employees
+                                </p>
+                            </div>
+                        </template>
 
-                    <UMeter :value="250" color="green" label="Permanent" icon="i-heroicons-cog-6-tooth" />
-                    <UMeter :value="40" color="yellow" label="Probation" icon="i-heroicons-window" />
-                    <UMeter :value="10" color="red" label="Contract" icon="i-heroicons-document" />
-                    <UMeter :value="20" color="gray" label="Intern" icon="i-heroicons-film" />
-                </UMeterGroup>
-            </main>
-        </UCard>
+                        <UMeter :value="250" color="green" label="Permanent" />
+                        <UMeter :value="40" color="yellow" label="Probation" />
+                        <UMeter :value="10" color="red" label="Contract" />
+                        <UMeter :value="20" color="gray" label="Intern" />
+                    </UMeterGroup>
+                </main>
+            </UCard>
+        </div>
+
+        <div class="col-span-2 p-3">
+            <UCard>
+                <BarChart :data="chartBarData" :options="chartOptions" />
+            </UCard>
+        </div>
+        <div class="p-3">
+            <UCard>
+                <DoughnutChart
+                    :data="chartDoughnutData"
+                    :options="chartOptions"
+                />
+            </UCard>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+const chartBarData = {
+    labels: ["January", "February", "March"],
+    datasets: [
+        {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [40, 20, 12],
+        },
+        {
+            label: "Data Two",
+            backgroundColor: "#f123f1",
+            data: [40, 20, 12],
+        },
+    ],
+};
+const chartOptions = {
+    responsive: true,
+};
+
+const chartDoughnutData = {
+    labels: ["Male", "Female"],
+    datasets: [
+        {
+            backgroundColor: ["#41B883", "#E46651"],
+            data: [112, 208],
+        },
+    ],
+};
 const items = [
     [
         {
