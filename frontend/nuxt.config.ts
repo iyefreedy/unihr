03ddd0ccced1fdf292d4ab8b019078536761e889
Muhammad Quraisy <quraisy@uai.ac.id>
2023-11-20 08:36:56 +0700

    fix: add utils to imports

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    runtimeConfig: {
+        public: {
+            backendUrl: "",
+            frontendUrl: "",
+        },
+    },
     typescript: {
         shim: false,
     },
@@ -10,9 +16,8 @@ export default defineNuxtConfig({
     colorMode: {
         preference: "light",
     },
-
     modules: ["@nuxt/ui"],
     imports: {
-        dirs: ["./types"],
+        dirs: ["./types", "./utils"],
     },
 });
