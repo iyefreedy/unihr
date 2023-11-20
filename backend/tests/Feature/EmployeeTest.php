9f302b98bb4f44a25b422320c0e759aee4ff3939
Muhammad Quraisy <quraisy@uai.ac.id>
2023-11-20 08:40:10 +0700

    add feature test

--- backend/tests/Feature/EmployeeTest.php
+++ backend/tests/Feature/EmployeeTest.php
@@ -4,11 +4,11 @@ namespace Tests\Feature;
 
 use App\Models\Employee;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class EmployeeTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      */
@@ -19,17 +19,21 @@ class EmployeeTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_create_employees(): void
+    public function test_create_employees_is_succeed(): void
     {
+        $this->seed();
+
         $employee = new Employee;
-        $employee->nik = '132104980010';
-        $employee->nip = '10.0.0.031';
+        $employee->identity_number = '132104980010';
         $employee->name = 'Muhammad Quraisy';
         $employee->birth_date = '1998-04-21';
         $employee->birth_place = 'Bogor';
         $employee->gender = 'male';
         $employee->phone = '087880688799';
         $employee->address = 'Gg. Tiang 2 No.52 RT.004/02';
+        $employee->city = 'Bogor';
+        $employee->province = 'Jawa Barat';
+        $employee->postal_code = '16921';
         $result = $employee->save();
 
         $this->assertTrue($result);
--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -3,18 +3,9 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        DB::delete("DELETE FROM employees");
-        DB::delete("DELETE FROM organizations");
-        DB::delete("DELETE FROM faculties");
-    }
 }
