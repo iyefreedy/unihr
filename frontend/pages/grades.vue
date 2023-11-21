e60c6c16071e1bf7733b24031a9a083a245b2a18
Muhammad Quraisy <quraisy@uai.ac.id>
2023-11-21 17:13:29 +0700

    refactor: add salary range columns

--- frontend/pages/grades.vue
+++ frontend/pages/grades.vue
@@ -15,9 +15,9 @@
             <main>
                 <div class="flex items-center justify-between mb-6">
                     <UInput placeholder="Search..." />
-                    <UButton class="ml-auto inline-block" label="New ranks" />
+                    <UButton class="ml-auto inline-block" label="New grade" />
                 </div>
-                <UTable :rows="grades" :columns="columns">
+                <UTable :rows="grades" :columns="gradecolumns">
                     <template #actions-data="{ row }">
                         <UDropdown :items="actionItems(row)">
                             <UButton
@@ -44,10 +44,24 @@
             <main>
                 <div class="flex items-center justify-between mb-6">
                     <UInput placeholder="Search..." />
-                    <UButton class="ml-auto inline-block" label="New ranks" />
+                    <UButton class="ml-auto inline-block" label="New rank" />
                 </div>
 
-                <UTable :rows="ranks" :columns="columns">
+                <UTable
+                    :rows="ranks"
+                    :columns="rankColumns"
+                    :ui="{
+                        tr: {
+                            base: 'hover:bg-gray-200 transition-colors duration-300 dark:hover:bg-gray-800',
+                        },
+                    }"
+                >
+                    <template #salary-data="{ row }">
+                        <UBadge color="teal"
+                            >{{ numberFormat(row.salary_from) }} -
+                            {{ numberFormat(row.salary_to) }}</UBadge
+                        >
+                    </template>
                     <template #actions-data="{ row }">
                         <UDropdown :items="actionItems(row)">
                             <UButton
@@ -63,7 +77,7 @@
 </template>
 
 <script lang="ts" setup>
-const columns = [
+const gradecolumns = [
     {
         key: "name",
         label: "Name",
@@ -71,6 +85,24 @@ const columns = [
     {
         key: "alias",
         label: "Alias",
+    },
+    {
+        key: "actions",
+    },
+];
+
+const rankColumns = [
+    {
+        key: "name",
+        label: "Name",
+    },
+    {
+        key: "alias",
+        label: "Alias",
+    },
+    {
+        key: "salary",
+        label: "Range Salary",
     },
     {
         key: "actions",
@@ -91,34 +123,50 @@ const ranks = [
     {
         name: "IA",
         alias: "Juru Muda",
+        salary_from: 1_560_800,
+        salary_to: 2_335_800,
     },
     {
         name: "IB",
         alias: "Juru Muda Tk.I",
+        salary_from: 1_704_500,
+        salary_to: 2_472_900,
     },
     {
         name: "IC",
         alias: "Juru",
+        salary_from: 1_776_600,
+        salary_to: 2_577_500,
     },
     {
         name: "ID",
         alias: "Juru Tk.I",
+        salary_from: 1_851_800,
+        salary_to: 2_686_500,
     },
     {
         name: "IIA",
         alias: "Pengatur Muda",
+        salary_from: 2_022_200,
+        salary_to: 3_373_600,
     },
     {
         name: "IIB",
         alias: "Pengatur Muda Tk.I",
+        salary_from: 2_208_400,
+        salary_to: 3_516_300,
     },
     {
         name: "IIC",
         alias: "Pengatur",
+        salary_from: 2_301_800,
+        salary_to: 3_665_000,
     },
     {
         name: "IID",
         alias: "Pengatur Tk.I",
+        salary_from: 2_399_200,
+        salary_to: 3_820_000,
     },
 ];
 
