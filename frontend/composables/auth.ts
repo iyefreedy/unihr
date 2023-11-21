1720fd9070679d2d7eee74a082db6816ddc838b1
Muhammad Quraisy <quraisy@uai.ac.id>
2023-11-21 17:11:31 +0700

    fix: error calling nuxt app instance out of scope

--- frontend/composables/auth.ts
+++ frontend/composables/auth.ts
@@ -1,26 +1,3 @@
-export type User = {
-    name: string;
-    email?: string;
-};
-
-export type LoginCredentials = {
-    email: string;
-    password: string;
-};
-
-export type RegisterCredentials = {
-    name: string;
-    email: string;
-    password: string;
-    password_confirmation: string;
-};
-
-export type ResetPasswordCredentials = {
-    email: string;
-    password: string;
-    password_confirmation: string;
-    token: string;
-};
 
 export const useAuth = <T = User>() => {
     const router = useRouter();
